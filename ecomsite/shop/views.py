8ef68f660a9d2bee54ef87c42e7794ccff4dc047
Omnish22 <omnishkumar@Omnishs-MacBook-Pro.local>
2023-08-15 20:14:38 +0530

    pagination added

---
+++ ecomsite/shop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Products
+from django.core.paginator import Paginator 
+
+# Create your views here.
+
+def index(request):
+    product_objects = Products.objects.all()
+    item_name = request.GET.get("item_name")
+
+    # SEARCH FUNCTIONALITY
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+
+    # PAGINATION
+    pagination = Paginator( product_objects, 4 )
+    page = request.GET.get('page') # get current page
+    product_objects = pagination.get_page(page)
+
+    return render(request, 'shop/index.html', {'product_objects':product_objects})
+
+ 
